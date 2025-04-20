72bc3d18dbc959dbfd1e3111df96dec6c2dfdfea
Krystian Wojtiuk <krystianwwork@gmail.com>
2025-04-20 15:05:17 +0100

    v2

--- app/test_main.py
+++ app/test_main.py
@@ -1,60 +1,72 @@
+from app.main import outdated_products
+import datetime
 from unittest.mock import patch
 import pytest
-import datetime
-from app.main import outdated_products
-from typing import Generator
 
 
-@pytest.fixture
-def products() -> list:
-    return [
-        {
-            "name": "salmon",
-            "expiration_date": datetime.date(2022, 2, 10),
-            "price": 600
-        },
-        {
-            "name": "chicken",
-            "expiration_date": datetime.date(2022, 2, 5),
-            "price": 120
-        },
-        {
-            "name": "duck",
-            "expiration_date": datetime.date(2022, 2, 1),
-            "price": 160
-        }
+@pytest.mark.parametrize(
+    "product_list, today_date, expected",
+    [
+        (
+            [
+                {
+                    "name": "salmon",
+                    "expiration_date": datetime.date(2022, 2, 10),
+                    "price": 600},
+                {
+                    "name": "chicken",
+                    "expiration_date": datetime.date(2022, 2, 5),
+                    "price": 120},
+                {
+                    "name": "duck",
+                    "expiration_date": datetime.date(2022, 2, 1),
+                    "price": 160
+                },
+            ],
+            datetime.date(2022, 2, 2),
+            ["duck"],
+        ),
+        (
+            [
+                {
+                    "name": "salmon",
+                    "expiration_date": datetime.date(2022, 2, 10),
+                    "price": 600},
+                {
+                    "name": "chicken",
+                    "expiration_date": datetime.date(2022, 2, 5),
+                    "price": 120},
+                {
+                    "name": "duck",
+                    "expiration_date": datetime.date(2022, 2, 1),
+                    "price": 160
+                },
+            ],
+            datetime.date(2023, 2, 2),
+            ["salmon", "chicken", "duck"],
+        ),
+        (
+            [
+                {
+                    "name": "salmon",
+                    "expiration_date": datetime.date(2022, 2, 10),
+                    "price": 600},
+                {
+                    "name": "chicken",
+                    "expiration_date": datetime.date(2022, 2, 5),
+                    "price": 120},
+                {
+                    "name": "duck",
+                    "expiration_date": datetime.date(2022, 2, 1),
+                    "price": 160
+                },
+            ],
+            datetime.date(2021, 2, 2),
+            [],
+        ),
     ]
-
-
-@pytest.fixture()
-def mocked_date() -> Generator:
-    with patch("app.main.datetime.date.today") as mock_date:
-        yield mock_date
-
-
-def test_date_earlier_than_all(
-        products: list,
-        mocked_date: Generator
-) -> None:
-    mocked_date.return_value = datetime.date(2022, 1, 1)
-    assert outdated_products(products) == [
-        "salmon",
-        "chicken",
-        "duck",
-    ]
-
-
-def test_date_later_than_all(
-        products: list,
-        mocked_date: Generator
-) -> None:
-    mocked_date.return_value = datetime.date(2023, 3, 1)
-    assert outdated_products(products) == []
-
-
-def test_date_equals_middle(
-        products: list,
-        mocked_date: Generator
-) -> None:
-    mocked_date.return_value = datetime.date(2022, 2, 5)
-    assert outdated_products(products) == ["salmon"]
+)
+@patch("app.main.datetime.date")
+def test_outdated_products(mock_date, product_list, today_date, expected):
+    mock_date.today.return_value = today_date
+    assert outdated_products(product_list) == expected
